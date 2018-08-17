1b7ea30acf84eafaa3b489da320bb2c90e1f18a4
Phil Howard <phil@gadgetoid.com>
2018-08-17 14:54:37 +0000

    Added Python3 install status checks

--- install.sh
+++ install.sh
@@ -38,8 +38,27 @@ if [[ "$COUNT" -eq "0" ]] || [[ "$DETECTED" == "" ]]; then
 	exit 1
 fi
 
+if [[ -f "/usr/bin/python3" ]]; then
+	DETECTED3=`python3 autodetect.py --install`
+fi
+
+array_index () {
+	local -n array=$1
+	string=$2
+	printf "$array $string\n"
+	for i in "${!array[@]}"; do
+		if [[ "${array[$i]}" = "$string" ]]; then
+			return $i
+		fi
+	done
+	return -1
+}
+
 check_status () {
 	index=0
+
+	# Iterate through Python3 detected packages
+	# and build out arrays of devices, statuses and Git repos
 	while read line; do
 		IFS='|' read -r -a package <<< "$line"
 		package_name=${package[0]}
@@ -50,6 +69,26 @@ check_status () {
 		REPOS[$index]=$package_library
 		index=$(($index+1))
 	done < <(echo -e "$DETECTED")
+
+	# Iterate through Python3 detected packages
+	# and update status to required if they are missing
+	if [[ ! "$DETECTED3" = "" ]]; then
+		while read line; do
+			IFS='|' read -r -a package <<< "$line"
+			package_name=${package[0]}
+			package_library=${package[1]}
+			package_status=${package[2]}
+
+			# Find the index of the package in our original array
+			# that we produced in the first loop above
+			array_index DEVICES "$package_name"
+			index=$?
+
+			if [[ ! "$index" == "-1" ]] && [[ "$package_status" == "required" ]]; then
+				STATUSES[$index]=$package_status
+			fi
+		done < <(echo -e "$DETECTED3")
+	fi
 }
 
 do_install () {
@@ -61,7 +100,7 @@ do_install () {
 	cd $TMP_DIR
 	if [[ ! -d "$git_dir/.git" ]]; then
 		rm -f $git_dir > $LOG_FILE 2>&1
-		git clone https://github.com/pimoroni/$line $git_dir > $LOG_FILE 2>&1
+		git clone https://github.com/pimoroni/$package_library $git_dir > $LOG_FILE 2>&1
 	fi
 	cd $git_dir
 	git pull origin master > $LOG_FILE 2>&1
